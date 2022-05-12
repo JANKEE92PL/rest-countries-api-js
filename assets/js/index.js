a9cd341e656f31b9e3d46ce6701b269ac421a499
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-05-12 16:09:04 +0200

    fetching data from api endpoint

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,19 @@ let flag = document.querySelector(".flag");
 flag.addEventListener("click", (e) => {
   window.location.pathname = "/show.html";
 });
+
+let searchForm = document.getElementById("searchForm");
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const search = document.querySelector('input[type="search"]');
+  searchValue = search.value;
+  fetchCountry(searchValue);
+});
+
+async function fetchCountry(keyword) {
+  let url = "https://restcountries.com/v2/name/";
+  let target = url.concat(keyword);
+  let response = await fetch(target);
+  let data = await response.text();
+  console.table(data);
+}
